44dd5fab04e621bf40e6ae84ebd70a750c50d923
Kristijan Šimenić <kristijan@xdoo.hr>
2022-06-06 08:04:46 +0200

    Add fetch user example

--- react-app/src/GoogleCallback.js
+++ react-app/src/GoogleCallback.js
@@ -6,8 +6,28 @@ function GoogleCallback() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [data, setData] = useState({});
+    const [user, setUser] = useState(null);
     const location = useLocation();
 
+    function fetchUserData() {
+        fetch(`http://localhost:80/api/user`, {
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + data.access_token,
+            }
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setUser(data);
+            })
+            .catch((error) => {
+                setLoading(false);
+                setError(error);
+            });
+    }
 
     useEffect(() => {
 
@@ -35,8 +55,17 @@ function GoogleCallback() {
     } else {
         if (error !== null) {
             return <DisplayError error={error}/>
+        } else if (user != null) {
+            return <DisplayUser user={user}/>
         } else {
-            return <DisplayData data={data}/>
+            return (
+                <div>
+                    <DisplayData data={data}/>
+                    <div style={{marginTop:10}}>
+                        <button onClick={fetchUserData}>Fetch User</button>
+                    </div>
+                </div>
+            );
         }
     }
 }
@@ -57,6 +86,14 @@ function DisplayData(data) {
     return (
         <div>
             <samp>{JSON.stringify(data, null, 2)}</samp>
+        </div>
+    );
+}
+
+function DisplayUser(user) {
+    return (
+        <div>
+            <samp>{JSON.stringify(user, null, 2)}</samp>
         </div>
     );
 }
